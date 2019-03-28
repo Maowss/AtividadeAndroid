dfa21235bdb4162d1e924416bcde531e35188d53
Maowss <Maowss@gmail.com>
2019-03-27 22:12:04 -0300

    Tudo android

---
+++ app/src/main/java/br/com/etecia/atividade/LoginBandioActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.atividade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginBandioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_bandio_layout);
+    }
+}
